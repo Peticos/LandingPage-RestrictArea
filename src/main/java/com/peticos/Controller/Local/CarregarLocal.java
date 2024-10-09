e066f82a9f81059efb4b8eaa43ee53d5f67a5fe1
Ruan Pelegrini <ruan.loureco@germinare.org.br>
2024-10-09 08:24:11 -0300

    regex local e mensagem de erro

--- src/main/java/com/peticos/Controller/Local/CarregarLocal.java
+++ src/main/java/com/peticos/Controller/Local/CarregarLocal.java
@@ -7,6 +7,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.sql.ResultSet;
@@ -37,6 +38,16 @@ public class CarregarLocal extends HttpServlet {
         } catch (SQLException e) {
             throw new ServletException("Erro ao carregar os locais", e);
         }
+
+        // Recuperar a mensagem da sessão, se existir
+        HttpSession session = request.getSession(false);
+        String message = (String) session.getAttribute("message");
+
+        // Remover a mensagem da sessão para que ela não fique persistente
+        session.removeAttribute("message");
+
+        // Passar a mensagem para o JSP
+        request.setAttribute("message", message);
 
         request.setAttribute("locais", locais);
         request.getRequestDispatcher("/areaRestrita/local/locais.jsp").forward(request, response);
