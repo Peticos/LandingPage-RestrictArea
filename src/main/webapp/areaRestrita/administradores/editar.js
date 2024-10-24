99e1e61260d49f7283f7b4abd360ca21bd14129b
Isaac Dias <isaac.dias@germinare.org.br>
2024-10-24 13:07:02 -0300

    Comentando e organizando os scripts

--- src/main/webapp/areaRestrita/administradores/editar.js
+++ src/main/webapp/areaRestrita/administradores/editar.js
@@ -1,5 +1,8 @@
+// Seleciona a div de editar que será aberta
 const editDiv = document.getElementsByClassName("edit-container")[0];
+
 $(".edit").click(function (){
+    // Preenche o form de editar com as informações do campo escolhido
     const id = Number(this.parentElement.parentElement.children[0].innerText);
 
     editDiv.children[0].querySelector("#nome-administrador-e").value = this.parentElement.parentElement.children[1].innerText;
--- src/main/webapp/areaRestrita/dicasDoDia/filtros.js
+++ src/main/webapp/areaRestrita/dicasDoDia/filtros.js
@@ -1,6 +1,7 @@
 const table = document.querySelector("table tbody");
 const rows = table.getElementsByTagName("tr");
 
+// Funções genéricas
 function limparDatas(){
     $("#data-inicio").val("")
     $("#data-fim").val("")
@@ -13,7 +14,8 @@ function limparFiltros(){
     limparID();
 }
 
-function filterTable() {
+// Funções de filtro
+function filtrarPorTexto() {
     limparFiltros();
 
     let input = document.querySelector(".pesquisar input"); // Input de pesquisa
@@ -33,7 +35,7 @@ function filterTable() {
         }
     }
 }
-function filterByDate() {
+function filtrarPorData() {
     const startDate = new Date(document.getElementById("data-inicio").value);
     const endDate = new Date(document.getElementById("data-fim").value);
 
@@ -42,9 +44,11 @@ function filterByDate() {
     for (let i = 1; i < rows.length; i++) {
         const dateCell = rows[i].getElementsByTagName("td")[4]; // Coluna de Data
         const dataTexto = dateCell.innerHTML;
+
         const dia = dataTexto.substring(0, 2);
         const mes = dataTexto.substring(3, 5);
         const ano = dataTexto.substring(6);
+
         const dataFormatada = ano + "-" + mes + "-" + dia;
         const rowDate = new Date(dataFormatada);
 
@@ -55,14 +59,20 @@ function filterByDate() {
         }
     }
 }
+function filtrarPorId() {
+    limparDatas();
 
-// Adicionar listeners para os inputs de data e um listener para o campo de pesquisa
-document.getElementById("data-inicio").addEventListener("change", filterByDate);
-document.getElementById("data-fim").addEventListener("change", filterByDate);
-document.querySelector(".pesquisar input").addEventListener("keyup", filterTable);
-
-// Dica De Hoje
-document.getElementById("dica-hoje").addEventListener("click", function (){
+    console.log(this.value)
+    for (let i = 1; i < rows.length; i++) {
+        if (rows[i].getElementsByTagName("td")[0].innerText!==this.value){
+            rows[i].style.display = "none";
+            console.log()
+        } else{
+            rows[i].style.display = "grid";
+        }
+    }
+}
+function mostrarDicaDeHoje() {
     limparFiltros();
 
     let hoje = new Date();
@@ -82,25 +92,8 @@ document.getElementById("dica-hoje").addEventListener("click", function (){
             rows[i].style.display = "grid";
         }
     }
-})
-
-// Filtrar pelo id
-$(".id")[0].querySelector("input").addEventListener("keyup", function (){
-    limparDatas();
-
-    console.log(this.value)
-    for (let i = 1; i < rows.length; i++) {
-        if (rows[i].getElementsByTagName("td")[0].innerText!==this.value){
-            rows[i].style.display = "none";
-            console.log()
-        } else{
-            rows[i].style.display = "grid";
-        }
-    }
-});
-
-// Parar os filtros
-document.getElementById("parar-filtro").addEventListener("click", function (){
+}
+function pararFiltros() {
     if ($('#open-btn').is(":checked")){
         limparFiltros();
 
@@ -108,4 +101,13 @@ document.getElementById("parar-filtro").addEventListener("click", function (){
             rows[i].style.display = "grid";
         }
     }
-})
+}
+
+
+// Event listeners
+document.getElementById("data-inicio").addEventListener("change", filtrarPorData);
+document.getElementById("data-fim").addEventListener("change", filtrarPorData);
+document.querySelector(".pesquisar input").addEventListener("keyup", filtrarPorTexto);
+document.getElementById("dica-hoje").addEventListener("click", mostrarDicaDeHoje);
+document.getElementsByClassName(".id")[0].querySelector("input").addEventListener("keyup", filtrarPorId);
+document.getElementById("parar-filtro").addEventListener("click", pararFiltros);
