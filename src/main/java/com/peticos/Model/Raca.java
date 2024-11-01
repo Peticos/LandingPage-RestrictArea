db4f06081545233eda398f8d1ef864d733f0a438
Pedro Frossard <pedro.frossard@germinare.org.br>
2024-11-01 08:52:03 -0300

    Arrumando problema da padronização

--- src/main/java/com/peticos/Model/Raca.java
+++ src/main/java/com/peticos/Model/Raca.java
@@ -9,6 +9,11 @@ public class Raca {
         this.raca = raca;
         this.idRaca = idRaca;
     }
+
+    public Raca(String raca) {
+        this.raca = raca;
+    }
+
     public String getRaca() {
         return this.raca;
     }
